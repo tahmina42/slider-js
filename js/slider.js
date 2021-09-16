6fdc7f25fbc731d022cd6743d7955d6d880bea39
tahmina42 <tahminamukta42@gmail.com>
2021-09-16 15:08:38 +0600

    first commit

---
+++ js/slider.js
@@ -0,0 +1,22 @@
+const images = [
+    'images/pic-1.jpg',
+    'images/pic-2.jpg',
+    'images/pic-3.jpg',
+    'images/pic-4.jpg',
+    'images/pic-5.jpg',
+    'images/pic-6.jpg'
+
+]
+let imgIndex = 0;
+const imgElement = document.getElementById("slider-img");
+setInterval(() => {
+
+    if (imgIndex >= images.length) {
+        imgIndex = 0;
+    }
+    const imgUrl = images[imgIndex];
+    imgElement.setAttribute('src', imgUrl);
+    imgIndex++;
+    console.log(imgUrl);
+
+}, 5000)
